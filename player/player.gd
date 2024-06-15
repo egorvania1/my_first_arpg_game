3fc1ac06ca276bc2dbc83b89ca2c6ecd039ab7ce
egorvania1 <egorvania1@gmail.com>
2024-06-15 19:30:40 +0500

    Enemy - Player collisions P1

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,6 @@ func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
 	
-
 func updateAnimation():
 	if velocity.length() == 0:
 		if animations.is_playing():
@@ -19,8 +18,14 @@ func updateAnimation():
 		elif velocity.y < 0: direction = "Up"
 		animations.play("walk" + direction)
 
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		print_debug(collider.name)
+
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	handleCollision()
 	updateAnimation()
-	
